329cac6ceadcff8bdcd18c92624ae5c86175c546
Sara Elsayed <sara.alsayed.moustafa@gmail.com>
2019-05-27 11:40:48 +0200

    Add Exams Controller

--- UI.Examica/UI.Examica.API/Dtos/RegisterDto.cs
+++ UI.Examica/UI.Examica.API/Dtos/RegisterDto.cs
@@ -11,7 +11,6 @@ namespace UI.Examica.API.Dtos
         [Required]
         public string Email { get; set; }
 
-        [Required]
         [StringLength(100, ErrorMessage = "PASSWORD_MIN_LENGTH", MinimumLength = 6)]
         public string Password { get; set; }
     }
