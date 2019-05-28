9f358a3f918e9b0e2519f8e498514dc147d18486
Aya ElNahas <ayaa.alnahas@gmail.com>
2019-05-28 15:37:06 +0200

    adding folder container and Form Component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import 'element-theme-default';
 import './App.css';
 import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 import Home from '../Button'
+import Register from '../../Containers/Account/Register'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Redirect from="/home" to="/"/>
         <Route path="/" exact component={Home}/>
+        <Route path="/register" exact component={Register}/>
         <Route render={()=>{return "not found!!!"}}/>
       </Switch>
     </BrowserRouter>
