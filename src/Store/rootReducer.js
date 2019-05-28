889e39122bdb143689b5044734c08551cfcda6a8
OmarZayed77 <zayedomarr@gmail.com>
2019-05-28 14:17:16 +0200

    Add API Folder with URL Config and add Exam Reducer

--- src/Store/rootReducer.js
+++ src/Store/rootReducer.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
-
+import ExamReducer from './Reducers/examReducer';
 
 const rootReducer = combineReducers(
     {
-       
+       exams: ExamReducer
     }
 );
 
