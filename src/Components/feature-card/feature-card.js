0737b5f2700a2cb9497abe05bb6d09818f146c74
fatma Alaa <fatma@iti.com>
2019-05-29 13:44:49 +0200

    feature-card, feature-card-list, axios downloaded, google fonts link

--- src/Components/feature-card/feature-card.js
+++ src/Components/feature-card/feature-card.js
@@ -1,46 +1,33 @@
-import React from 'react';
-import './feature-card.css';
-import {Button, Layout, Card} from 'element-react/next';
+import React from "react";
+import "./feature-card.css";
+import { Layout, Card } from "element-react/next";
 
-const FeatureCard = () => {
-  return( <Layout.Row gutter="5" type="flex" justify="center" >
-    <Layout.Col span={ 7 }>
-      <Card bodyStyle={{ padding: 0 }}>
-        <img src="https://i.ibb.co/jrSgZK6/undraw-time-management-30iu.png" alt="timeIcon"/>
-        <div style={{ padding: 14 }}>
-          <span>Timed examsr</span>
-          <div className="bottom clearfix">
-            <p>set a time for how long you expect a question to be answered
-                  that will help you keep a timeframe for the hole exam. </p>
-          </div>
-        </div>
-      </Card>
-    </Layout.Col>
-    <Layout.Col span={ 7 }>
-      <Card bodyStyle={{ padding: 0 }}>
-        <img src="https://i.ibb.co/jrSgZK6/undraw-time-management-30iu.png" alt="icon"/>
-        <div style={{ padding: 14 }}>
-          <span>Timed exams</span>
-          <div className="bottom clearfix">
-          <p>set a time for how long you expect a question to be answered
-                  that will help you keep a timeframe for the hole exam. </p>
-          </div>
-        </div>
-      </Card>
-    </Layout.Col>
-    <Layout.Col span={ 7 }>
-      <Card bodyStyle={{ padding: 0 }}>
-        <img src="https://i.ibb.co/jrSgZK6/undraw-time-management-30iu.png" alt="icon"/>
-        <div style={{ padding: 14 }}>
-          <span>Timed exams</span>
-          <div className="bottom clearfix">
-          <p>set a time for how long you expect a question to be answered
-                  that will help you keep a timeframe for the hole exam. </p>
-          </div>
-        </div>
-      </Card>
-    </Layout.Col>
-  </Layout.Row>);
-};
+const FeatureCard = props=> {
+    return (
+      <Layout.Row
+        className="FeatureCard"
+        gutter="10"
+        type="flex"
+        justify="center"
+      >
+        <Layout.Col span={5}>
+          <Card bodyStyle={{ padding: 0 }}>
+            <img
+              className="FeatureCard-Image"
+              src={this.props.image}
+              alt="timeIcon"
+            />
+            <div style={{ padding: 14 }}>
+              <span className="FeatureCard-Title">{this.props.title}</span>
+              <div className="bottom clearfix">
+                <p className="FeatureCard-Content">{this.props.content}</p>
+              </div>
+            </div>
+          </Card>
+        </Layout.Col>
+      </Layout.Row>
+    );
+
+}
 
 export default FeatureCard;
