babb9bc359e0b2c13153e92c4450fa75ce41ea56
OmarZayed77 <zayedomarr@gmail.com>
2019-05-29 13:34:41 +0200

    Add Exam Reducer and API

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import 'element-theme-default';
 import './App.css';
 import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
-import Home from '../Button'
+import AddExam from '../../Containers/exams/exam-add'
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/exams/add" component={AddExam}/>
         <Redirect from="/home" to="/"/>
-        <Route path="/" exact component={Home}/>
+        <Route path="/" exact render={()=>{return "Home Sweet Home"}}/>
         <Route render={()=>{return "not found!!!"}}/>
       </Switch>
     </BrowserRouter>
