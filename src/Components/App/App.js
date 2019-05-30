14924e285106b2eb8ad5441fb4159280306c3501
fatma Alaa <fatma@iti.com>
2019-05-30 11:46:30 +0200

    features card added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,41 +1,37 @@
-import React from 'react';
-import 'element-theme-default';
-import './App.css';
-import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
-<<<<<<< HEAD
-//import Home from '../why-to-use/why-to-use';
-import FeatureCard from '../feature-card/feature-card';
-=======
-import Register from '../../Containers/Account/Register'
-import QuestionSideNave from '../../Containers/QuestionSideNav';
->>>>>>> fac506ea9ed66e40b97553dd8f82a63f7aabe90d
-import AddExam from '../../Containers/exams/exam-add'
-import CreateQuestions from '../../Containers/Questions-list';
-import Login from '../../Containers/Account/Login'
+import React from "react";
+import "element-theme-default";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import FeatureCardList from "../../Containers/feature-card-list/feature-card-list";
+// import Register from "../../Containers/Account/Register";
+// import QuestionSideNave from "../../Containers/QuestionSideNav";
+// import AddExam from "../../Containers/exams/exam-add";
+// import CreateQuestions from "../../Containers/Questions-list";
+// import Login from "../../Containers/Account/Login";
 
 function App() {
   return (
     <BrowserRouter>
-     <div className="App">
-       <div className="container">
-      <Switch>
-        <Route path="/exams/add" component={AddExam}/>
-        <Redirect from="/home" to="/"/>
-<<<<<<< HEAD
-        <Route path="/" exact component={FeatureCard}/>
-=======
->>>>>>> fac506ea9ed66e40b97553dd8f82a63f7aabe90d
-        <Route path="/register" exact component={Register}/>
-        <Route path="/QuestionSideNave" exact component={QuestionSideNave}/>
-        <Route path="/login" exact component={Login}/>
-        <Route path="/CreateQuestions" exact component={CreateQuestions}/>
-<<<<<<< HEAD
-=======
-        <Route path="/" exact render={()=>{return "Home Sweet Home"}}/>
->>>>>>> fac506ea9ed66e40b97553dd8f82a63f7aabe90d
-        <Route render={()=>{return "not found!!!"}}/>
-      </Switch>
-      </div>
+      <div className="App">
+        <div className="container">
+          <Switch>
+            {/* <Route path="/exams/add" component={AddExam} /> */}
+            <Route path="/" exact component={FeatureCardList} />
+            {/* <Route path="/register" exact component={Register} />
+            <Route
+              path="/QuestionSideNave"
+              exact
+              component={QuestionSideNave}
+            />
+            <Route path="/login" exact component={Login} />
+            <Route path="/CreateQuestions" exact component={CreateQuestions} />
+            <Route
+              render={() => {
+                return "not found!!!";
+              }}
+            /> */}
+          </Switch>
+        </div>
       </div>
     </BrowserRouter>
   );
