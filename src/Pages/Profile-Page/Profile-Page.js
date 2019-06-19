725cb939bb280da7788177ee16de8baaac3d99b7
fatma Alaa <fatma@iti.com>
2019-06-19 14:52:27 +0200

    profile page finished

--- src/Pages/Profile-Page/Profile-Page.js
+++ src/Pages/Profile-Page/Profile-Page.js
@@ -1,30 +1,57 @@
 import React, { Component, Fragment } from "react";
-import { Layout, Icon } from "element-react";
+import { Layout } from "element-react";
 import { connect } from "react-redux";
-import * as userActions from '../../Store/Actions/userActions'
+import * as userActions from "../../Store/Actions/userActions";
 import "./Profile-Page.css";
 
 class ProfilePage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeUser: {
+        name: "fatma",
+        email: "fatma@gmail.com",
+        phone: "01159751361"
+      }
+    };
+  }
+
+  // componentDidMount() {
+  //   this.props.getUser(this.props.userId, "1", this.props.token);
+  // }
+
   render() {
     return (
       <Fragment>
-        <Layout.Row>
+        <Layout.Row className="ProfilePage">
           <Layout.Col span={5}>
-            <div className="Person-pic" />
+            <Layout.Row>
+              <Layout.Col className="ProfilePage-left-section">
+                <div className="ProfilePage-pic" />
+              </Layout.Col>
+            </Layout.Row>
+            <Layout.Row>
+              <Layout.Col>
+                <div className="ProfilePage-info">
+                  <h3 className="ProfilePage-info-name">{this.state.activeUser.name}</h3>
+                  <p>
+                  <i className="fas fa-envelope ProfilePage-info-icon"></i>
+                    {this.state.activeUser.email}
+                  </p>
+                  <p>
+                    <i className="fas fa-phone ProfilePage-info-icon" />
+                    {this.state.activeUser.phone}
+                  </p>
+                </div>
+              </Layout.Col>
+            </Layout.Row>
           </Layout.Col>
-        </Layout.Row>
-        <Layout.Row>
-          <Layout.Col span={5}>
-            <div className="Person-info">
-              <p>Ahmed Sallam</p>
-              <p>
-                <Icon name="message" />
-              </p>
-              <p>
-                <i class="fas fa-phone" />
-              </p>
-            </div>
-          </Layout.Col>
+        
+        <Layout.Col span={17} className="ProfilePage-right-section">
+          <div>test 1</div>
+          <div>test 2</div>
+        </Layout.Col>
         </Layout.Row>
       </Fragment>
     );
@@ -33,13 +60,19 @@ class ProfilePage extends Component {
 
 const mapStateToProps = state => {
   return {
-    token: state.auth.token
-  }
-}
+    token: state.auth.token,
+    userId: state.auth.userId
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
-    getUser: (userId, orgId,token)=>{dispatch(userActions.getOneUser(userId,orgId,token)) }
+    getUser: (userId, orgId, token) => {
+      dispatch(userActions.getOneUser(userId, orgId, token));
+    }
   };
 };
-export default connect(null,mapDispatchToProps)(ProfilePage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProfilePage);
