44fa97e64db994acb590863f410b19fff9430c1f
OmarZayed77 <zayedomarr@gmail.com>
2019-06-20 16:37:41 +0200

    Edit footer links

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Menu } from 'element-react/next';
 import './Footer.css';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter} from 'react-router-dom';
 
 const Footer = () => {
 
