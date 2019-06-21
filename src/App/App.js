fa7960508e063bbd227b67b985db5d7e15bb5928
fatma Alaa <fatma@iti.com>
2019-06-21 18:16:27 +0200

    complex question added

--- src/App/App.js
+++ src/App/App.js
@@ -2,21 +2,23 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { setToken } from "../Store/Actions/authActions";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Loading } from "element-react/next";
 import "element-theme-default";
-import Nav from "./../Components/Navbar/Navbar";
+import Nav from "../Components/Navbar/Navbar";
 import Profile from "../Pages/Profile-Page/Profile-Page";
 import Register from "../Containers/Account/Register/Register";
 import Login from "../Containers/Account/Login/Login";
 import LogOut from "../Components/LogoutButton/LogoutButton";
 import Footer from "../Components/Footer";
 import PricingPlan from "../Pages/PricingPlan";
-import * as orgActions from "../Store/Actions/organizationActions";
-import Exam from '../Containers/exams/exam-list/exam-list';
-import AboutUs from '../Components/AboutUs';
-import "./App.css";
+import Exam from "../Containers/exams/exam-list/exam-list";
+import AboutUs from "../Components/AboutUs";
 import Home from "../Pages/Home-Page";
 import AddOrganization from "../Containers/AddOrgnization";
-import { Loading } from "element-react/next";
+import x from "../Containers/CreateComprehenssionQuestion";
+import y from "../Containers/CreateChoiseQuestion";
+import * as orgActions from "../Store/Actions/organizationActions";
+import "./App.css";
 
 class App extends Component {
   componentDidMount() {
@@ -44,6 +46,8 @@ class App extends Component {
             <Route path="/Pricing" component={PricingPlan} />
             <Route path="/exam" component={Exam} />
             <Route path="/aboutus" component={AboutUs} />
+            <Route path="/y" component={y} />
+            <Route path="/x" component={x} />
             <Route path="/" component={Home} />
             <Route
               render={() => {
